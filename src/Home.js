087d6d3d10f98068bd5ac90816a735ad2b1a23d2
Badal Moradia <badal@i360llc.com>
2018-08-01 18:07:19 -0500

    rendered hello,world on the page, changed file from App to Home

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+// Developer TODO: Define App component defintion here
+
+
+import React, { Component } from 'react';
+
+class Home extends Component {
+	render() {
+		return (
+			<h4>Hello, World!</h4>
+		)
+	}
+}
+
+
+export default Home
